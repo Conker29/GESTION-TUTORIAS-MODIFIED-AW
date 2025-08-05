2623a084cb865110c7c7ba4457f07dd6650cd0cb
dannamishelle23 <dannamishelle.23@gmail.com>
2025-08-05 16:57:51 -0500

    administrador.js fixed
    modelo arreglado

--- backend/src/models/administrador.js
+++ backend/src/models/administrador.js
@@ -1,65 +1,3 @@
-/*import {Schema, model} from 'mongoose'
-import bcrypt from 'bcryptjs'
-
-const administradorSchema = new Schema({
-    nombreAdministrador: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
-    }, 
-    password: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: Boolean,
-        default: true,
-    },
-    token: {
-        type: String,
-        default: null,
-    },
-    confirmEmail: {
-        type: Boolean,
-        default: true,
-    },
-    rol: {
-        type: String,
-        default: "Administrador"
-    }
-}, {
-    timestamps: true,
-})
-
-//Metodo para cifrar el password
-administradorSchema.methods.encrypPassword = async function(password) {
-    const salt = await bcrypt.genSalt(10)
-    const passwordEncryp = await bcrypt.hash(password, salt)
-    return passwordEncryp
-}
-
-//Metodo para verificar si el password ingresado es el mismo en la BDD
-administradorSchema.methods.matchPassword = async function(password) {
-    const response = await bcrypt.compare(password, this.password)
-    return response
-}   
-
-//Metodo para crear un token
-administradorSchema.methods.crearToken = function() {
-    const tokenGenerado = this.token = Math.random().toString(32).slice(2)
-    return tokenGenerado
-}
-
-export default model('Administrador', administradorSchema)*/
-
-//MODELO ADMINISTRADOR MODIFICADO PARA USAR AUTENTICACIÓN OAUTH
-
 import { Schema, model } from 'mongoose'
 import bcrypt from 'bcryptjs'
 
@@ -80,6 +18,10 @@ const administradorSchema = new Schema({
     required: function () {
       return !this.isOAuth; // solo requerido si no es OAuth
     }
+  },
+  fotoPerfil: {
+    type: String,
+    default: "https://cdn-icons-png.flaticon.com/512/4715/4715329.png"  //Enviar un icono por defecto
   },
   status: {
     type: Boolean,
